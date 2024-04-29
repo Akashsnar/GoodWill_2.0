f103cc3b88f8e12261d25754a47581e478f55eef
Dhruvrajwebdeveloper <dhruvcoolraj@gmail.com>
2024-04-29 17:43:51 +0530

    cart backend creation

--- backend/routes/CartPostRoutes.js
+++ backend/routes/CartPostRoutes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const expressAsyncHandler = require('express-async-handler');
-const productlisting = require('../mongoSchema/Products');
-router.post('/cartdetails',expressAsyncHandler(async(req,res)=>{
+const {handleCart} = require('../controllers/cartController');
+router.post('/cartdetails',handleCart);
+// router.post('/cartdetails',(req,res)=>{
+//     console.log('h')
+// });
 
-    const payload = req.body;
-    console.log("je");
-    console.log(req.body);
-    res.status(200).send(req.body)
-
-}));
 module.exports=router;
